2fddb14f2c7aed3572b7bd5f531cf7107ff91938
YoungchoKim <dudch91@naver.com>
2024-08-08 21:54:37 +0900

    Time: 7 ms (86.9%), Space: 13.8 MB (23.17%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-
-    int map[300][300];
-    int offset = 101;
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         vector<vector<int>> ans;
         int dy[] = {0, 1, 0, -1};
         int dx[] = {1, 0, -1, 0};
         int ans_cnt = 0;
-        for(int i = 0 ; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                map[offset + i][offset + j] = 1;
-            }
-        }
-
         int cnt = 1;
         int dir = 0;
         ans.push_back({rStart, cStart});
@@ -23,10 +14,11 @@ public:
                 for(int j = 0 ; j < cnt; j++){
                     rStart += dy[dir];
                     cStart += dx[dir];
-                    if (map[rStart + offset][cStart+offset]){
-                        ans.push_back({rStart, cStart});
-                        ans_cnt++;
+                    if (rStart < 0 || rStart >= rows || cStart < 0 || cStart >= cols){
+                        continue;
                     }
+                    ans.push_back({rStart, cStart});
+                    ans_cnt++;
                 }
                 dir = (dir + 1) % 4;
             }
