7ff788486642ec999a04f8b245c7e9705f030ce5
YoungchoKim <dudch91@naver.com>
2024-10-19 20:33:40 +0900

    Time: 19 ms (48.29%), Space: 11.9 MB (54.27%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -5,6 +5,7 @@ public:
         int size = 1;
         for(int i = 0 ; i < n ; i++){
             int s = size;
+            if (k <= s) return arr[k-1] + '0';
             arr[s++] = 1;
             for(int i = size-1; i >= 0; i--){
                 arr[s++] = arr[i] ^ 1;
