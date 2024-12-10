3c839d86629e9ec3a0a828a9e2cd5da2eb7217e1
YoungchoKim <dudch91@naver.com>
2024-12-10 22:54:30 +0900

    Time: 4 ms (91.14%), Space: 8.9 MB (96.76%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -4,19 +4,15 @@ public:
         int ans = -1;
         int l = 1;
         int r = s.size()-2;
+        
         while(l <= r){
-            int mid = (l + r) / 2;
-            cout << l << " " << mid << " " << r << endl;
+            int mid = (l + r) >> 1;
             bool success = false;
             for(char i = 'a'; i <= 'z'; i++){
-                string ss = "";
-                for(int j = 0 ; j < mid; j++){
-                    ss += i;
-                }
                 int k = 0;
                 int specialCnt = 0;
                 for(int j = 0; j < s.size(); j++){
-                    if(s[j] == ss[k]){
+                    if(s[j] == i){
                         k++;
                     } else{
                         k = 0;
@@ -29,7 +25,6 @@ public:
                             break;
                         }
                     }
-                    if(success) break;
                 }
                 if(success) break;
             }
