befc3bba687ddaf4abab025d2c918c5ff0600c0e
YoungchoKim <dudch91@naver.com>
2024-12-19 22:06:44 +0900

    Time: 0 ms (100%), Space: 9.1 MB (35.22%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rightMin[10];
+    vector<int> st;
+    int maxChunksToSorted(vector<int>& arr) {
+        int ans = 0;
+        for(int i = arr.size()-1; i >= 0; i--){
+            if(st.empty()){
+                st.push_back(arr[i]);
+                rightMin[i] = arr.size();
+            } else if(arr[i] < st.back()){
+                rightMin[i] = st.back();
+                st.push_back(arr[i]);
+            }
+        }
+        int leftMax = 0;
+        for(int i = 0 ; i < arr.size(); i++){
+            leftMax = max(leftMax, arr[i]);
+            if(leftMax < rightMin[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
