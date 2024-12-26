a63fe3abb8f971e901e592d46434f650062fa804
YoungchoKim <dudch91@naver.com>
2024-12-26 22:47:16 +0900

    Time: 4 ms (81.21%), Space: 11.8 MB (64.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    int memo[20][2001];
     int findTargetSumWays(vector<int>& nums, int target, int cur=0, int sum=0) {
+        if(cur == 0){
+            memset(memo, -1, sizeof(memo));
+        }
         if(cur == nums.size()){
             if (sum == target){
                 return 1;
             }
             return 0;
         }
+        if(memo[cur][sum+1000] != -1) return memo[cur][sum+1000];
         int res = 0;
         res += findTargetSumWays(nums, target, cur+1, sum + nums[cur]);
         res += findTargetSumWays(nums, target, cur+1, sum - nums[cur]);
-        return res;
+        return memo[cur][sum+1000] = res;
     }
 };
